3f97ea271a18b4c84761c55b9bf834c6f4633d7d
Augusto Laurino <augustolaurino10@gmail.com>
2024-02-26 17:40:31 -0300

    atualizando script com api viacep

--- validador_cep.py
+++ validador_cep.py
@@ -1,23 +1,34 @@
-import re
+import requests
 
-
-def validar_cep(cep):
-    if not (100000 < int(cep) < 999999):
-        return False
-
-    if re.search(r'(\d)(?=\d\1)', cep):
-        return False
-
-    return True
-
+def obter_endereco(cep):
+    url = 'https://viacep.com.br/ws/{cep}/json/'.format(cep=cep)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if 'erro' in data:
+            return None
+        else:
+            return data
+    else:
+        return None
 
 def main():
-    cep = input('Digite o CEP a ser validado: ')
-    if validar_cep(cep):
-        print('CEP válido!')
+    cep = input('Digite o CEP a ser consultado: ').strip()
+    endereco = obter_endereco(cep)
+    if endereco:
+        print(f'O CEP {cep} é válido\n')
+        print('Detalhes do Endereço:')
+        print('CEP:', endereco['cep'])
+        print('Logradouro:', endereco['logradouro'])
+        print('Complemento:', endereco['complemento'])
+        print('Bairro:', endereco['bairro'])
+        print('Localidade:', endereco['localidade'])
+        print('UF:', endereco['uf'])
+        print('IBGE:', endereco['ibge'])
+        print('DDD:', endereco['ddd'])
+        print('SIAFI:', endereco['siafi'])
     else:
-        print('CEP inválido!')
-
+        print('CEP inválido.')
 
 if __name__ == '__main__':
     main()
