bf071e03c4183cd2c97f3c5d88085f870dc718b2
Augusto Laurino <augustolaurino10@gmail.com>
2024-02-28 19:38:11 -0300

    conectando com banco

--- validador_cep.py
+++ validador_cep.py
@@ -1,7 +1,18 @@
 import requests
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_password = os.getenv("DATABASE_PASSWORD")
+
+client = MongoClient('mongodb+srv://admin:' + db_password + '@cluster0.w9iiuwu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client['ceps_validados'] 
+collection = db['cep']  
 
 def obter_endereco(cep):
-    url = 'https://viacep.com.br/ws/{cep}/json/'.format(cep=cep)
+    url = f"https://viacep.com.br/ws/{cep}/json/"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -13,10 +24,16 @@ def obter_endereco(cep):
         return None
 
 def main():
-    cep = input('Digite o CEP a ser consultado: ').strip()
-    endereco = obter_endereco(cep)
+    cep = input('Digite o CEP a ser consultado: ')
+
+    if '-' not in cep:
+        cep_com_hifen = f"{cep[:5]}-{cep[5:]}"
+    else:
+        cep_com_hifen = cep
+
+    endereco = obter_endereco(cep_com_hifen)
+
     if endereco:
-        print(f'O CEP {cep} é válido\n')
         print('Detalhes do Endereço:')
         print('CEP:', endereco['cep'])
         print('Logradouro:', endereco['logradouro'])
@@ -27,8 +44,14 @@ def main():
         print('IBGE:', endereco['ibge'])
         print('DDD:', endereco['ddd'])
         print('SIAFI:', endereco['siafi'])
+
+        if collection.find_one({'cep': cep_com_hifen}) is None:
+            collection.insert_one(endereco)
+            print('CEP gravado com sucesso!')
+        else:
+            print('Este CEP já está registrado na coleção.')
     else:
-        print('CEP inválido.')
+        print('CEP INVÁLIDO.')
 
 if __name__ == '__main__':
     main()
